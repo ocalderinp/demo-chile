256ba9af4bf574cb31eb11f007be895d9160b77a
Oscar Calderin <ocalderin@hg.com.uy>
2019-05-14 17:03:38 -0300

    commit inicial

---
+++ src/test/java/fligths/utils/WebDriverManager.java
@@ -0,0 +1,48 @@
+package fligths.utils;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.ie.InternetExplorerDriver;
+
+public class WebDriverManager {
+    private WebDriver driver = null;
+    private static GetProperties properties = new GetProperties();
+    private static String browser = properties.getString("BROWSER").toUpperCase();
+
+    public WebDriver getDriver(){
+        if(driver == null){
+            driver = createDriver();
+        }
+        return driver;
+    }
+
+    private WebDriver createDriver() {
+        if (browser.equals("CHROME")){
+            driver = new ChromeDriver(getChromeOptions());
+        } else if(browser.equals("FIREFOX")){
+            driver = new FirefoxDriver();
+        }  else if(browser.equals("IE")){
+            driver = new InternetExplorerDriver();
+        }
+        else {
+            System.out.println("Navegdar seleccionado no soportado. Se utiliza chrome por defecto");
+            driver = new ChromeDriver(getChromeOptions());
+        }
+        return driver;
+    }
+
+    private ChromeOptions getChromeOptions(){
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("--disable-extensions");
+        options.addArguments("disable-infobars");
+        return options;
+    }
+
+    public void quitDriver(){
+        if(driver != null){
+            driver.quit();
+        }
+    }
+}
